ec4151bdd27e0b523ce972004a6d2932d9cc82f4
Iyad M. Guesba <iyad.guesba@icloud.com>
2024-04-13 23:33:08 +0200

    Première Avancé du Projet Ajout des classes GrapheLArcs.java GrapheMAdj.java création de la classe Arc qui représente un arc avec constructeur et attributs d'un arc et premier code de la classe GrapheLArcs on peut y voir une list<Arc> ainsi que des getter et la fonction ajouterArc

--- graphe/GrapheLArcs.java
+++ graphe/GrapheLArcs.java
@@ -1,10 +1,7 @@
 package graphe;
 
-import graphe.Arc;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class GrapheLArcs {
     private List<Arc> arcs;
